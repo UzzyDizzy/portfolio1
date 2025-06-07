85c6c79ffa3ccb42b794ca60768e6c765e02e357
UzzyDizzy <uditsenapaty@gmail.com>
2025-06-07 10:31:42 +0530

    first commit

---
+++ src/contents/projects.ts
@@ -0,0 +1,29 @@
+import { Project } from "@/types";
+
+
+export const projects: Project[] = [
+    {
+        title: 'Maze Solving using Q Learning',
+        description: 'A 2D Maze Solving Q Learning agent with Reinforcement Learning',
+        technologies: ['Python', 'Pygame', 'Tinkler', 'Jupyter'],
+        githubLink: 'https://github.com/UzzyDizzy/MazeQLearning',
+        demoLink: 'https://demo.com',
+        image: '/projects/ql3.jpeg',
+      },
+      {
+        title: 'Gacha Games Tracker',
+        description: 'A community game tracking application with guides and interaction (under development).',
+        technologies: ['Next', 'Tailwind', 'Supabase', 'Framer Motion'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/gg.png',
+      },
+      {
+        title: 'Portfolio Website',
+        description: 'My personal portfolio website showcasing my projects and skills.',
+        technologies: ['Next.js', 'Tailwind CSS', 'Framer Motion'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/portfolio-website.jpg',
+      },
+  ];
